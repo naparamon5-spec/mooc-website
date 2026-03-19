b9e84f7f456f655d4ccea4e4e616b1e006d1da28
naparamon5-spec <naparamon5@gmail.com>
2026-03-19 21:26:22 +0800

    fix: update the content

--- components/FeatureCards.vue
+++ components/FeatureCards.vue
@@ -3,22 +3,22 @@
   <div class="w-full px-4 md:px-8 lg:px-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
       <Card class="bg-blue-100">
         <div class="text-center">
-          <h3 class="text-xl font-semibold text-primary-600 mb-1">10 modules</h3>
-          <p class="text-sm text-muted">Gain competency-based skills</p>
+          <h3 class="text-xl font-semibold text-primary-600 mb-1">5 modules</h3>
+          <p class="text-sm text-muted">for the Beginner Course</p>
         </div>
       </Card>
 
       <Card class="bg-blue-100">
         <div class="text-center">
-          <h3 class="text-xl font-semibold text-primary-600 mb-1">Beginner Course</h3>
-          <p class="text-sm text-muted">No prior experience required</p>
+          <h3 class="text-xl font-semibold text-primary-600 mb-1">5 modules</h3>
+          <p class="text-sm text-muted">for the Advanced Course</p>
         </div>
       </Card>
 
       <Card class="bg-blue-100">
         <div class="text-center">
-          <h3 class="text-xl font-semibold text-primary-600 mb-1">1 month to complete</h3>
-          <p class="text-sm text-muted">at 2 hours a week</p>
+          <h3 class="text-xl font-semibold text-primary-600 mb-1">1 week to complete</h3>
+          <p class="text-sm text-muted">at 2 hours a day</p>
         </div>
       </Card>
 
